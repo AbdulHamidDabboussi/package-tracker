545d40c04eca59f3749bffb2aefe312954ca7a8b
msafadieh <me@msafadieh.com>
2018-11-08 12:48:21 -0500

    add empty package_tracker module

---
+++ package_tracker.py
@@ -0,0 +1,8 @@
+"""
+package_tracker(tracking_number, tracking_service)
+
+NOT IMPLEMENTED YET
+"""
+
+def package_tracker(tracking_number=None, tracking_service=None):
+    raise NotImplementedError("package_tracker() not implemented yet")
