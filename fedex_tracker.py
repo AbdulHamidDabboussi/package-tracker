50c3834beb43d2a58f15d7a3195bcf323a73a8a4
msafadieh <me@msafadieh.com>
2018-11-08 12:35:58 -0500

    clean up, add fedex_tracker()

--- fedex_tracker.py
+++
@@ -1,5 +0,0 @@
-import xml.etree.ElementTree as ET
-import requests
-
-KEY, PASSWORD, ACC_NUMBER, MET_NUMBER  = open("sec.txt").read().splitlines()
-TEMPLATE = ET.parse("template.xml")
--- fedex_tracker/fedex_tracker.py
+++ fedex_tracker/fedex_tracker.py
@@ -1,3 +1,19 @@
+import xml.etree.ElementTree as ET
 import requests
 
 KEY, PASSWORD, ACC_NUMBER, MET_NUMBER  = open("sec.txt").read().splitlines()
+TEMPLATE = ET.parse("template.xml")
+
+def fedex_tracker(tracking_number):
+    request = TEMPLATE
+    root = request.getroot()
+    root[1][0][0][0][0].text = root[1][0][0][1][0].text = KEY
+    root[1][0][0][0][1].text = root[1][0][0][1][1].text = PASSWORD
+    root[1][0][1][0].text = ACC_NUMBER
+    root[1][0][1][1].text = MET_NUMBER
+    root[1][0][4][1][1].text = str(tracking_number)
+    xml = ET.tostring(root)
+    headers = {'Content-Type': 'application/xml'}
+    url = ""
+    xml_reply = requests.post('url', data=xml, headers=headers).text
+    return 
