5255b78f784cc0310a63db6478851b3448e8dd40
msafadieh <me@msafadieh.com>
2018-11-08 22:12:24 -0500

    finish usps tracker and parser

--- usps_tracker/usps_tracker.py
+++ usps_tracker/usps_tracker.py
@@ -3,10 +3,17 @@ usps_tracker
 
 Tracks USPS pacakages.
 """
+import xml.etree.ElementTree as ET
 import requests
 
 def make_template(tracking_number):
-    template = open("template.xml","r").read()
+    """
+    Generates XML template necessary for request.
+    Contains:
+        * username (from sec.txt)
+        * tracking number (input)
+    """
+    template = open("template.xml", "r").read()
 
     user = open("sec.txt").read().splitlines()[0]
 
@@ -14,11 +21,27 @@ def make_template(tracking_number):
 
     return template
 
+def usps_response_parser(response):
+    """
+    Parses the response received from USPS.
+    Returns a dictionary with tracking and number and status, USPS isn't fancy.
+    """
+    response_xml = ET.fromstring(response)
+
+    return {
+        "TRACKING_NUMBER": response_xml[0].attrib['ID'],
+        "STATUS": response_xml[0][0].text
+    }
+
 def usps_tracker(tracking_number):
+    """
+    Tracks USPS package. Uses make_template to generate XML request and then
+    parses it using usps_response_parser.
+    """
     request_xml = make_template(tracking_number)
-    
+
     url = f"https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML={request_xml}"
 
-    request = requests.post(url)
+    response = requests.post(url).text
 
-    return request.text
+    return usps_response_parser(response)
